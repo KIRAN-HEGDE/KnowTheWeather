aa1156bcabe7adcd80b808d6c8252e8a2d43440f
prajwalrajbr <rajprajwal3@gmail.com>
2021-12-09 15:28:59 +0530

    Added Database

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(20), nullable=False)
+    lastName = db.Column(db.String(20))
+    email = db.Column(db.String(40), nullable=False, primary_key=True)
+    password = db.Column(db.String(40), nullable=False)
+    location = db.Column(db.String(40))
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return '<Task %r>' % self.id
 
 @app.route('/')
 def index():
