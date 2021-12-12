10f0a27013ea2b09ff186fb12623e90cdff0156f
prajwalrajbr <42075029+prajwalrajbr@users.noreply.github.com>
2021-12-12 12:04:22 +0530

    Added User Authentication

--- app.py
+++ app.py
@@ -1,38 +1,14 @@
-from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+from . import db
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    firstName = db.Column(db.String(20), nullable=False)
-    lastName = db.Column(db.String(20))
-    email = db.Column(db.String(40), nullable=False, primary_key=True)
-    password = db.Column(db.String(40), nullable=False)
-    location = db.Column(db.String(40))
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self) -> str:
-        return '<Task %r>' % self.id
+app = Blueprint('app', __name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route('/register')
-def register():
-    return render_template('register.html')
-
 @app.route('/details')
+@login_required
 def weatherDetails():
-    return render_template('weatherDetails.html')
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return render_template('weatherDetails.html', name=current_user.firstName)
---
+++ model.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from flask_login import UserMixin
+from . import db
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(20), nullable=False)
+    lastName = db.Column(db.String(20))
+    email = db.Column(db.String(40), nullable=False, unique=True)
+    password = db.Column(db.String(40), nullable=False)
+    location = db.Column(db.String(40))
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return '<Task %r>' % self.id
